d8c9c893f65ac424b3f59d362414eab2dfb0f100
@mohammedamir.khan <mohammedamir.khan@npstx.com>
2025-08-03 16:45:26 +0530

    ssl problem fixed

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-// server.js
-const fs = require("fs");
-const path = require("path");
-const https = require("https");
-const next = require("next");
+// // server.js
+// const fs = require("fs");
+// const path = require("path");
+// const https = require("https");
+// const next = require("next");
 
-const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// const port = parseInt(process.env.PORT, 10) || 3000;
+// const dev = process.env.NODE_ENV !== "production";
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
 
-// Load your SSL certificate and key
-const httpsOptions = {
-  key: fs.readFileSync(path.join(__dirname, "ssl", "key.pem")),
-  cert: fs.readFileSync(path.join(__dirname, "ssl", "cert.pem")),
-};
+// // Load your SSL certificate and key
+// const httpsOptions = {
+//   key: fs.readFileSync(path.join(__dirname, "ssl", "key.pem")),
+//   cert: fs.readFileSync(path.join(__dirname, "ssl", "cert.pem")),
+// };
 
-app.prepare().then(() => {
-  https
-    .createServer(httpsOptions, (req, res) => {
-      handle(req, res);
-    })
-    .listen(port, () => {
-      console.log(`✅ Server ready at https://localhost:${port}`);
-    });
-});
+// app.prepare().then(() => {
+//   https
+//     .createServer(httpsOptions, (req, res) => {
+//       handle(req, res);
+//     })
+//     .listen(port, () => {
+//       console.log(`✅ Server ready at https://localhost:${port}`);
+//     });
+// });
